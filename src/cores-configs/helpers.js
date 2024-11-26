4f12b5fe8900fe0daeaaeecdb1f39516dd5312a9
张喜 <zhangxi@acctrue.com>
2024-11-26 18:04:18 +0800

    cf.090227.xyz

--- src/cores-configs/helpers.js
+++ src/cores-configs/helpers.js
@@ -33,6 +33,7 @@ export async function getConfigAddresses(hostName, cleanIPs, enableIPv6) {
     }
 
     return [
+        'cf.090227.xyz',
         'speed.marisalnc.com',
         'shopify.com',
         ...resolved.ipv4,
